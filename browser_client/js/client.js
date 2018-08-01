b6f575bb9c44b23cf1695d5bc181725d6003f701
Serg-Maximchuk <serg.maximchuk@gmail.com>
2018-08-02 02:18:30 +0300

    Implemented smooth movement with mock server

--- browser_client/js/client.js
+++ browser_client/js/client.js
@@ -5,6 +5,8 @@ const KEY_UP = 38;
 const KEY_RIGHT = 39;
 const KEY_DOWN = 40;
 
+const walkStepTime = 0;
+
 (function () {
 
     let screenHeight = window.innerHeight - 4; // extra 4px, prevents scroll bar, dunno why
@@ -12,7 +14,9 @@ const KEY_DOWN = 40;
 
     let playerSettings = {
         x: screenWidth / 2,
-        y: screenHeight / 2
+        y: screenHeight / 2,
+        isMovingX: false,
+        isMovingY: false,
     };
 
     let playground = document.getElementById('playground');
@@ -25,35 +29,132 @@ const KEY_DOWN = 40;
         return Math.min(max, Math.max(min, value));
     }
 
-    function goDown() {
-        playerSettings.y = limit(0, screenHeight, playerSettings.y + 5);
+    let ws = new WebSocket('ws://localhost:8080/ws');
+
+    const topicToAction = {
+        moveX: function (delta) {
+            if (playerSettings.isMovingX) return;
+
+            playerSettings.isMovingX = true;
+
+            setTimeout(function moveX() {
+                if (playerSettings.isMovingX) {
+                    playerSettings.x = limit(0, screenWidth, playerSettings.x + delta);
+                    setTimeout(moveX, walkStepTime);
+                }
+            }, walkStepTime);
+        },
+        moveY: function (delta) {
+            if (playerSettings.isMovingY) return;
+
+            playerSettings.isMovingY = true;
+
+            setTimeout(function moveY() {
+                if (playerSettings.isMovingY) {
+                    playerSettings.y = limit(0, screenHeight, playerSettings.y + delta);
+                    setTimeout(moveY, walkStepTime);
+                }
+            }, walkStepTime);
+        },
+        stopMoveX: function () {
+            playerSettings.isMovingX = false;
+        },
+        stopMoveY: function () {
+            playerSettings.isMovingY = false;
+        }
+    };
+
+    ws.onmessage = function (event) {
+        topicToAction[event.data.topic](event.data.payload);
+    };
+
+    const controlBytes = {
+        goUp: 1,
+        goDown: 2,
+        goLeft: 3,
+        goRight: 4,
+        stopUp: 5,
+        stopDown: 6,
+        stopLeft: 7,
+        stopRight: 8
+    };
+
+    function sendByte(value) {
+        const buffer = new ArrayBuffer(1);
+        buffer[0] = value;
+        ws.send(buffer);
     }
 
-    function goUp() {
-        playerSettings.y = limit(0, screenHeight, playerSettings.y - 5);
+    function sendUp() {
+        playerSettings.isMovingY || sendByte(controlBytes.goUp);
     }
 
-    function goLeft() {
-        playerSettings.x = limit(0, screenWidth, playerSettings.x - 5);
+    function sendDown() {
+        playerSettings.isMovingY || sendByte(controlBytes.goDown);
     }
 
-    function goRight() {
-        playerSettings.x = limit(0, screenWidth, playerSettings.x + 5);
+    function sendLeft() {
+        playerSettings.isMovingX || sendByte(controlBytes.goLeft);
     }
+
+    function sendRight() {
+        playerSettings.isMovingX || sendByte(controlBytes.goRight);
+    }
+
+    function sendStopUp() {
+        sendByte(controlBytes.stopUp);
+    }
+
+    function sendStopDown() {
+        sendByte(controlBytes.stopDown);
+    }
+
+    function sendStopLeft() {
+        sendByte(controlBytes.stopLeft);
+    }
+
+    function sendStopRight() {
+        sendByte(controlBytes.stopRight);
+    }
+
+    const movingKeysHolder = {
+        moveStart: {
+            38: sendUp,
+            40: sendDown,
+            37: sendLeft,
+            39: sendRight,
+        },
+        moveStop: {
+            38: sendStopUp,
+            40: sendStopDown,
+            37: sendStopLeft,
+            39: sendStopRight,
+        },
+        isMoveKey: function (key) {
+            return (key === KEY_DOWN)
+                || (key === KEY_UP)
+                || (key === KEY_LEFT)
+                || (key === KEY_RIGHT)
+        },
+        getMove: function (key) {
+            return this.moveStart[key];
+        },
+        getMoveStop: function (key) {
+            return this.moveStop[key];
+        }
+    };
 
     document.addEventListener('keydown', function (e) {
         let key = e.which || e.keyCode;
+        console.log(key);
+        if (!movingKeysHolder.isMoveKey(key)) return;
+        movingKeysHolder.getMove(key)();
+    });
 
-        switch (key) {
-            case KEY_DOWN:
-                return goDown();
-            case KEY_UP:
-                return goUp();
-            case KEY_LEFT:
-                return goLeft();
-            case KEY_RIGHT:
-                return goRight();
-        }
+    document.addEventListener('keyup', function (e) {
+        let key = e.which || e.keyCode;
+        if (!movingKeysHolder.isMoveKey(key)) return;
+        movingKeysHolder.getMoveStop(key)();
     });
 
     (function gameLoop() {
