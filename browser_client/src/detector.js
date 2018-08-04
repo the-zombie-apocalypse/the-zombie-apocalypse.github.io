f1483131f11f9b03f1b31dce956d8ea556977bfc
Serg-Maximchuk <serg.maximchuk@gmail.com>
2018-08-04 18:41:36 +0300

    Configured live reload

--- browser_client/src/detector.js
+++ browser_client/src/detector.js
@@ -3,14 +3,17 @@
  * @author mr.doob / http://mrdoob.com/
  */
 
-var Detector = {
+const Detector = {
 
     canvas: !!window.CanvasRenderingContext2D,
     webgl: (function () {
 
         try {
-            var canvas = document.createElement('canvas');
-            return !!(window.WebGLRenderingContext && (canvas.getContext('webgl') || canvas.getContext('experimental-webgl')));
+            let canvas = document.createElement('canvas');
+            return !!(
+                window.WebGLRenderingContext
+                && (canvas.getContext('webgl') || canvas.getContext('experimental-webgl'))
+            );
 
         } catch (e) {
             return false;
@@ -20,20 +23,19 @@ var Detector = {
     webgl2: (function () {
 
         try {
-            var canvas = document.createElement('canvas');
+            let canvas = document.createElement('canvas');
             return !!(window.WebGL2RenderingContext && (canvas.getContext('webgl2')));
 
         } catch (e) {
             return false;
         }
-
     })(),
     workers: !!window.Worker,
     fileapi: window.File && window.FileReader && window.FileList && window.Blob,
 
     getWebGLErrorMessage: function () {
 
-        var element = document.createElement('div');
+        let element = document.createElement('div');
         element.id = 'webgl-error-message';
         element.style.fontFamily = 'monospace';
         element.style.fontSize = '13px';
@@ -48,31 +50,20 @@ var Detector = {
         if (!this.webgl) {
 
             element.innerHTML = window.WebGLRenderingContext ? [
-                'Your graphics card does not seem to support <a href="http://khronos.org/webgl/wiki/Getting_a_WebGL_Implementation" style="color:#000">WebGL</a>.<br />',
+                'Your graphics card does not seem to support ' +
+                '<a href="http://khronos.org/webgl/wiki/Getting_a_WebGL_Implementation" style="color:#000">WebGL</a>' +
+                '.<br />',
                 'Find out how to get it <a href="http://get.webgl.org/" style="color:#000">here</a>.'
             ].join('\n') : [
-                'Your browser does not seem to support <a href="http://khronos.org/webgl/wiki/Getting_a_WebGL_Implementation" style="color:#000">WebGL</a>.<br/>',
+                'Your browser does not seem to support ' +
+                '<a href="http://khronos.org/webgl/wiki/Getting_a_WebGL_Implementation" style="color:#000">WebGL</a>' +
+                '.<br/>',
                 'Find out how to get it <a href="http://get.webgl.org/" style="color:#000">here</a>.'
             ].join('\n');
         }
 
         return element;
-    },
-
-    addGetWebGLMessage: function (parameters) {
-
-        var parent, id, element;
-
-        parameters = parameters || {};
-
-        parent = parameters.parent !== undefined ? parameters.parent : document.body;
-        id = parameters.id !== undefined ? parameters.id : 'oldie';
-
-        element = Detector.getWebGLErrorMessage();
-        element.id = id;
-
-        parent.appendChild(element);
     }
 };
 
-module.exports = Detector;
+export default Detector;
