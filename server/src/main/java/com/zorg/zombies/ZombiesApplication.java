95d5164efc0f90ef255ab8a8ff346ce2f04e9813
Serg-Maximchuk <serg.maximchuk@gmail.com>
2018-08-05 21:08:11 +0300

    Created empty server module

---
+++ server/src/main/java/com/zorg/zombies/ZombiesApplication.java
@@ -0,0 +1,12 @@
+package com.zorg.zombies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZombiesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZombiesApplication.class, args);
+    }
+}
