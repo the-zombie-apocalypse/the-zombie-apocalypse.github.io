f0a4fae3d5e0b6fd54feee9f2d3d17bbc7601ae6
Serg-Maximchuk <serg.maximchuk@gmail.com>
2018-08-05 17:50:10 +0300

    Refactored main client js

--- browser_client/src/client.js
+++ browser_client/src/client.js
@@ -1,15 +1,13 @@
 import {AxesHelper} from 'three'
-import WebSocket from './mock_server'
 import './style.css'
 import Graphon from './graphics/graphon'
-import Player from "./entities/player";
-import global from './entities/global'
+import Player from './entities/player'
+import gameActions from './components/game_actions'
 
 export default function gameLoop() {
-    const KEY_LEFT = 37;
-    const KEY_UP = 38;
-    const KEY_RIGHT = 39;
-    const KEY_DOWN = 40;
+
+    gameActions.connectToServer();
+    gameActions.initKeyboardListener(window.document);
 
     const graphon = new Graphon(window);
     graphon.initGraph();
@@ -17,137 +15,8 @@ export default function gameLoop() {
     const player = new Player();
     graphon.addDynamic(player);
 
-    const playerSettings = global.playerSettings;
-
-    let axesHelper = new AxesHelper(150);
+    const axesHelper = new AxesHelper(150);
     graphon.addStatic(axesHelper);
-
-    const ws = new WebSocket('ws://localhost:8080/ws');
-
-    const topicToAction = {
-        moveX: function (delta) {
-            if (playerSettings.isMovingX) return;
-
-            playerSettings.isMovingX = true;
-
-            setTimeout(function moveX() {
-                if (playerSettings.isMovingX) {
-                    playerSettings.x = playerSettings.x + delta;
-                    setTimeout(moveX, playerSettings.walkStepTime);
-                }
-            }, playerSettings.walkStepTime);
-        },
-        moveY: function (delta) {
-            if (playerSettings.isMovingY) return;
-
-            playerSettings.isMovingY = true;
-
-            setTimeout(function moveY() {
-                if (playerSettings.isMovingY) {
-                    playerSettings.y = playerSettings.y - delta;
-                    setTimeout(moveY, playerSettings.walkStepTime);
-                }
-            }, playerSettings.walkStepTime);
-        },
-        stopMoveX: function () {
-            playerSettings.isMovingX = false;
-        },
-        stopMoveY: function () {
-            playerSettings.isMovingY = false;
-        }
-    };
-
-    ws.onmessage = function (event) {
-        topicToAction[event.data.topic](event.data.payload);
-    };
-
-    const controlBytes = {
-        goUp: 1,
-        goDown: 2,
-        goLeft: 3,
-        goRight: 4,
-        stopUp: 5,
-        stopDown: 6,
-        stopLeft: 7,
-        stopRight: 8
-    };
-
-    function sendByte(value) {
-        const buffer = new ArrayBuffer(1);
-        buffer[0] = value;
-        ws.send(buffer);
-    }
-
-    function sendUp() {
-        playerSettings.isMovingY || sendByte(controlBytes.goUp);
-    }
-
-    function sendDown() {
-        playerSettings.isMovingY || sendByte(controlBytes.goDown);
-    }
-
-    function sendLeft() {
-        playerSettings.isMovingX || sendByte(controlBytes.goLeft);
-    }
-
-    function sendRight() {
-        playerSettings.isMovingX || sendByte(controlBytes.goRight);
-    }
-
-    function sendStopUp() {
-        sendByte(controlBytes.stopUp);
-    }
-
-    function sendStopDown() {
-        sendByte(controlBytes.stopDown);
-    }
-
-    function sendStopLeft() {
-        sendByte(controlBytes.stopLeft);
-    }
-
-    function sendStopRight() {
-        sendByte(controlBytes.stopRight);
-    }
-
-    const movingKeysHolder = {
-        moveStart: {
-            38: sendUp,
-            40: sendDown,
-            37: sendLeft,
-            39: sendRight,
-        },
-        moveStop: {
-            38: sendStopUp,
-            40: sendStopDown,
-            37: sendStopLeft,
-            39: sendStopRight,
-        },
-        isMoveKey: function (key) {
-            return (key === KEY_DOWN)
-                || (key === KEY_UP)
-                || (key === KEY_LEFT)
-                || (key === KEY_RIGHT)
-        },
-        getMove: function (key) {
-            return this.moveStart[key];
-        },
-        getMoveStop: function (key) {
-            return this.moveStop[key];
-        }
-    };
-
-    document.addEventListener('keydown', function (e) {
-        let key = e.which || e.keyCode;
-        if (!movingKeysHolder.isMoveKey(key)) return;
-        movingKeysHolder.getMove(key)();
-    });
-
-    document.addEventListener('keyup', function (e) {
-        let key = e.which || e.keyCode;
-        if (!movingKeysHolder.isMoveKey(key)) return;
-        movingKeysHolder.getMoveStop(key)();
-    });
 
     (function gameLoop() {
         graphon.update();
