c2b05154066df6d90eca99feb70b032b16346821
Serg-Maximchuk <serg.maximchuk@gmail.com>
2018-08-05 15:35:30 +0300

    Extracted global object

---
+++ browser_client/src/entities/global.js
@@ -0,0 +1,11 @@
+const global = {
+    playerSettings: {
+        x: 0,
+        y: 0,
+        isMovingX: false,
+        isMovingY: false,
+        walkStepTime: 0
+    }
+};
+
+export default global;
