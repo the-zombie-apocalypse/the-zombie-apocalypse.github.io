3fa5ec0c1ff78cc93dc2238b18ebacba65af59c8
Serg-Maximchuk <serg.maximchuk@gmail.com>
2018-09-05 00:06:44 +0300

    Exception class renamed

--- server/src/main/java/com/zorg/zombies/command/exception/WrongUserIdException.java
+++
@@ -1,7 +0,0 @@
-package com.zorg.zombies.command.exception;
-
-public class WrongUserIdException extends RuntimeException {
-    public WrongUserIdException(String userId) {
-        super("Wrong user id exception: " + userId);
-    }
-}
