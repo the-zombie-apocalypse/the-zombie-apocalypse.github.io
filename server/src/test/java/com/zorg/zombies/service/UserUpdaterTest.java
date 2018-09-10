10eba651a16c7c5c0ab5e35fb73de098b18bb3db
Serg-Maximchuk <serg.maximchuk@gmail.com>
2018-09-11 00:11:12 +0300

    Covered move scenario with test case

--- server/src/test/java/com/zorg/zombies/service/UserUpdaterTest.java
+++ server/src/test/java/com/zorg/zombies/service/UserUpdaterTest.java
@@ -1,15 +1,23 @@
 package com.zorg.zombies.service;
 
+import com.zorg.zombies.change.UserChange;
 import com.zorg.zombies.change.UserMovingChange;
 import com.zorg.zombies.change.UserStopMovingChange;
+import com.zorg.zombies.model.MoveDirection;
 import com.zorg.zombies.model.MoveDirectionX;
 import com.zorg.zombies.model.MoveDirectionY;
 import com.zorg.zombies.model.MoveDirectionZ;
 import com.zorg.zombies.model.User;
+import javafx.util.Pair;
+import lombok.val;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+import java.util.function.BiConsumer;
+import java.util.function.BiFunction;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -185,5 +193,66 @@ class UserUpdaterTest {
         var userStopMovingChange = (UserStopMovingChange) userChange;
 
         assertEquals(userStopMovingChange.getStopMoveDirection(), userAlreadyMoves);
+    }
+
+    @Test
+    void moveScenario() {
+        val user = new User("id");
+
+        BiFunction<User, MoveDirection, UserChange> updateMove = userUpdater::updateUserMove;
+        BiFunction<User, MoveDirection, UserChange> updateStopMove = userUpdater::updateUserStopMove;
+
+        final List<Pair<BiFunction<User, MoveDirection, UserChange>, MoveDirection>> scenario = List.of(
+                new Pair<>(updateMove, MoveDirectionX.WEST),
+                new Pair<>(updateMove, MoveDirectionY.SOUTH),
+                new Pair<>(updateStopMove, MoveDirectionX.WEST),
+                new Pair<>(updateMove, MoveDirectionX.EAST),
+                new Pair<>(updateStopMove, MoveDirectionY.SOUTH),
+                new Pair<>(updateMove, MoveDirectionY.NORTH),
+                new Pair<>(updateStopMove, MoveDirectionX.EAST),
+                new Pair<>(updateStopMove, MoveDirectionY.NORTH)
+        );
+
+        final BiConsumer<UserChange, MoveDirection> checkUserMoveChange = (userChange, moveDirection) -> {
+            assertTrue(user.isMoving(moveDirection));
+            assertTrue(userChange.isUpdated());
+            assertTrue(userChange instanceof UserMovingChange);
+
+            var userMovingChange = (UserMovingChange) userChange;
+
+            assertEquals(userMovingChange.getMoveDirection(), moveDirection);
+        };
+
+        final BiConsumer<UserChange, MoveDirection> checkUserStopMoveChange = (userChange, moveDirection) -> {
+            assertFalse(user.isMoving(moveDirection));
+            assertTrue(userChange.isUpdated());
+            assertTrue(userChange instanceof UserStopMovingChange);
+
+            var userStopMovingChange = (UserStopMovingChange) userChange;
+
+            assertEquals(userStopMovingChange.getStopMoveDirection(), moveDirection);
+        };
+
+        final List<BiConsumer<UserChange, MoveDirection>> expects = List.of(
+                checkUserMoveChange,
+                checkUserMoveChange,
+                checkUserStopMoveChange,
+                checkUserMoveChange,
+                checkUserStopMoveChange,
+                checkUserMoveChange,
+                checkUserStopMoveChange,
+                checkUserStopMoveChange
+        );
+
+        for (int i = 0; i < scenario.size(); i++) {
+            final Pair<BiFunction<User, MoveDirection, UserChange>, MoveDirection> entry = scenario.get(i);
+            final BiFunction<User, MoveDirection, UserChange> changeBiFunction = entry.getKey();
+            final MoveDirection direction = entry.getValue();
+
+            final BiConsumer<UserChange, MoveDirection> directionBiConsumer = expects.get(i);
+            final UserChange userChange = changeBiFunction.apply(user, direction);
+
+            directionBiConsumer.accept(userChange, direction);
+        }
     }
 }
