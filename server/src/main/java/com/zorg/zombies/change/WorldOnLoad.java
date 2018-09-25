43d4c2ddc54e81641cb610946a8e949d37474d95
Serg-Maximchuk <serg.maximchuk@gmail.com>
2018-09-25 23:26:35 +0300

    Extended WorldOnLoad change.

--- server/src/main/java/com/zorg/zombies/change/WorldOnLoad.java
+++ server/src/main/java/com/zorg/zombies/change/WorldOnLoad.java
@@ -1,8 +1,15 @@
 package com.zorg.zombies.change;
 
 import com.zorg.zombies.model.Coordinates;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 
+@Data
+@EqualsAndHashCode(callSuper = true)
 public class WorldOnLoad extends WorldChange {
+
+    private boolean isGreeting = true;
+
     public WorldOnLoad(UserChange user) {
         super(user);
     }
