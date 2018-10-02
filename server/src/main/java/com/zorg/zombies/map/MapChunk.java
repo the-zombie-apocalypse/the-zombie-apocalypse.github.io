05bb9024d8e88d7ce36a06e731b5c5f0219aae8e
Serg-Maximchuk <serg.maximchuk@gmail.com>
2018-10-03 00:08:52 +0300

    MapChunk refactoring and test coverage

--- server/src/main/java/com/zorg/zombies/map/MapChunk.java
+++ server/src/main/java/com/zorg/zombies/map/MapChunk.java
@@ -4,25 +4,10 @@ import com.zorg.zombies.change.WorldChange;
 import com.zorg.zombies.model.Coordinates;
 import reactor.core.CoreSubscriber;
 
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
+public interface MapChunk {
+    void addObject(String id, Coordinates coordinates, CoreSubscriber<WorldChange> subscriber);
 
-public class MapChunk {
+    void notifyUsers(WorldChange change);
 
-    public static final int DEFAULT_WIDTH = 1024;
-    public static final int DEFAULT_HEIGHT = 1024;
-
-    private Map<String, CoreSubscriber<WorldChange>> userIdToSubscriber = new ConcurrentHashMap<>();
-
-    public void addObject(String id, Coordinates coordinates, CoreSubscriber<WorldChange> subscriber) {
-        userIdToSubscriber.put(id, subscriber);
-    }
-
-    public void notifyUsers(WorldChange change) {
-        userIdToSubscriber.values().forEach(subscriber -> subscriber.onNext(change));
-    }
-
-    public void unregister(String id) {
-        userIdToSubscriber.remove(id);
-    }
+    void unregister(String id);
 }
