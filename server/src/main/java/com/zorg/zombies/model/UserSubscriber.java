4189abf94abccf6a26b24d61b6948c7a400c5308
Serg-Maximchuk <serg.maximchuk@gmail.com>
2018-10-10 17:01:44 +0300

    Double "goodbye" problem resolved

--- server/src/main/java/com/zorg/zombies/model/UserSubscriber.java
+++ server/src/main/java/com/zorg/zombies/model/UserSubscriber.java
@@ -6,20 +6,13 @@ import lombok.Setter;
 import reactor.core.publisher.FluxProcessor;
 import reactor.core.publisher.ReplayProcessor;
 
-import javax.security.auth.Destroyable;
-
 @Getter
 @Setter
-public class UserSubscriber extends UserData implements Destroyable {
+public class UserSubscriber extends UserData {
 
     protected final FluxProcessor<WorldChange, WorldChange> subscriber = ReplayProcessor.create(256);
 
     public UserSubscriber(String id, Coordinates coordinates) {
         super(id, coordinates);
-    }
-
-    @Override
-    public void destroy() {
-//        subscriber.onComplete();
     }
 }
