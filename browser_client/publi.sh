e834ac66132a42a1db6aa0215ef1a9bf68059a2d
Serg-Maximchuk <serg.maximchuk@gmail.com>
2020-04-12 22:36:21 +0300

    Created hint publish files

---
+++ browser_client/publi.sh
@@ -0,0 +1,6 @@
+aws ecr get-login-password --region us-east-1
+docker login -u AWS 736762908522.dkr.ecr.us-east-1.amazonaws.com/zombies-front
+npm run build:prod
+docker build -t zombies-front .
+docker tag zombies-front:latest 736762908522.dkr.ecr.us-east-1.amazonaws.com/zombies-front:latest
+docker push 736762908522.dkr.ecr.us-east-1.amazonaws.com/zombies-front:latest
