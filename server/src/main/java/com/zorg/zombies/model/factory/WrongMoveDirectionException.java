84d4b939a5758efe744e282c9f3d1680d7fd92c1
Serg-Maximchuk <serg.maximchuk@gmail.com>
2020-04-22 11:36:16 +0300

    Introduced NoActionCommand

--- server/src/main/java/com/zorg/zombies/model/factory/WrongMoveDirectionException.java
+++
@@ -1,7 +0,0 @@
-package com.zorg.zombies.model.factory;
-
-public class WrongMoveDirectionException extends RuntimeException {
-    public WrongMoveDirectionException(String direction) {
-        super("Wrong user move direction: " + direction);
-    }
-}
