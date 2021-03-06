93dbc2b37418399e3033b2634748b4c08c53dfbf
Serg-Maximchuk <serg.maximchuk@gmail.com>
2021-03-06 22:12:49 +0200

    Prepared stage branch

--- server/src/main/java/com/zorg/zombies/change/NoUserChange.java
+++
@@ -1,8 +0,0 @@
-package com.zorg.zombies.change;
-
-public class NoUserChange extends UserChange {
-
-    public NoUserChange(String id) {
-        super(id, false);
-    }
-}
--- server/src/main/java/com/zorg/zombies/command/exception/UserIdIsRequired.java
+++
@@ -1,7 +0,0 @@
-package com.zorg.zombies.command.exception;
-
-public class UserIdIsRequired extends RuntimeException {
-    public UserIdIsRequired(String userId) {
-        super("Wrong user id exception: " + userId);
-    }
-}
